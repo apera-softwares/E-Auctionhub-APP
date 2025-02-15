e2d3270c698b3738cf557c9722cfcc5158566ac7
DilipLovevanshi <dilip@aperasoftwares.com>
2025-02-15 18:11:22 +0530

    working on auction details

--- app/auctionDetails.tsx
+++ app/auctionDetails.tsx
@@ -5,29 +5,51 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { APP_COLOR } from "constants/Colors";
+import { WebView } from "react-native-webview";
+import { useEffect, useState } from "react";
+import { BACKEND_API } from "constants/api";
+import dayjs from "dayjs";
 
 const AuctionDetails = () => {
   const { auctionId } = useLocalSearchParams() as any;
-  const isPremiumUser = false; // Change this based on user subscription
+  const isPremiumUser = false;
+  const [auctionDetails, setAuctionDetails] = useState({} as any);
+  const [loading, setLoading] = useState(true);
 
-  const auctionData = {
-    location: "Manewada, Nagpur",
-    assetType: "Flat",
-    bankName: "Bank of India",
-    area: "1500 sqft",
-    state: "Maharashtra",
-    city: "Nagpur",
-    applicationDate: "2024-03-15",
-    applicationDeadline: "2024-04-15",
-    loanAccountNumber: "1234-5678-9101",
-    bankContactPerson: "John Doe",
-    propertyAddress: "123 Main Street, Nagpur",
-    documents: "Confidential Docs.pdf",
-    earnestMoneyDeposit: "500,000 INR",
-    reservePrice: "2,500,000 INR",
+  const getAuctionById = async () => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNtMmU5czcxMDAwMDExMjFrM3JpcGp6MnMiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTczOTYwNzE2NH0.MUYiJIr54ykn1-hKv0G8EfJozYa2kwQMR7HJKLZzrEg";
+    let headers: any = {};
+    try {
+      setLoading(true);
+      if (token) {
+        headers["Authorization"] = `Bearer ${token}`;
+      }
+      const URL = `${BACKEND_API}auction/get/${auctionId}`;
+      const response = await fetch(URL, {
+        headers,
+      });
+      const data = await response.json();
+      console.log(data);
+
+      if (data.statusCode === 200) {
+        setAuctionDetails(data?.data);
+      }
+    } catch (error) {
+      console.log("error while fetching searched by id  auctions", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const googleMapsEmbedUrl = `https://maps.google.com/maps?q=${auctionDetails.latitude},${auctionDetails.longitude}&hl=es&z=14&amp;output=embed`;
+
+  useEffect(() => {
+    getAuctionById();
+  }, []);
 
   return (
     <ScrollView style={styles.container}>
@@ -35,53 +57,64 @@ const AuctionDetails = () => {
         <Text style={styles.title}>Auction Details</Text>
         <View style={styles.actionsContainer}>
           <TouchableOpacity>
-            <FontAwesome5 name="heart" size={24} color="#007bff" />
+            <FontAwesome5 name="heart" size={24} color={APP_COLOR.primary} />
           </TouchableOpacity>
           <TouchableOpacity>
-            <FontAwesome5 name="share-alt" size={24} color="#007bff" />
+            <FontAwesome5
+              name="share-alt"
+              size={24}
+              color={APP_COLOR.primary}
+            />
           </TouchableOpacity>
           <TouchableOpacity>
-            <FontAwesome5 name="copy" size={24} color="#007bff" />
+            <FontAwesome5 name="copy" size={24} color={APP_COLOR.primary} />
           </TouchableOpacity>
         </View>
         <View style={styles.fieldContainer}>
           <Text style={styles.field}>
-            <FontAwesome5 name="map-marker-alt" size={20} /> Location:{" "}
-            {auctionData.location}
-          </Text>
-          <Text style={styles.field}>
             <FontAwesome5 name="building" size={20} /> Asset Type:{" "}
-            {auctionData.assetType}
+            {auctionDetails.assetType}
           </Text>
           <Text style={styles.field}>
-            <FontAwesome5 name="university" size={20} /> Bank Name:{" "}
-            {auctionData.bankName}
+            <FontAwesome5 name="ruler-combined" size={20} /> Area (in square
+            feet) : {auctionDetails.areaSqFt}
           </Text>
-          <Text style={styles.field}>
-            <FontAwesome5 name="ruler-combined" size={20} /> Area:{" "}
-            {auctionData.area}
-          </Text>
-          <Text style={styles.field}>
-            <FontAwesome5 name="flag" size={20} /> State: {auctionData.state}
-          </Text>
-          <Text style={styles.field}>
-            <FontAwesome5 name="city" size={20} /> City: {auctionData.city}
-          </Text>
-          <Text style={styles.field}>
-            <FontAwesome5 name="calendar-alt" size={20} /> Application Date:{" "}
-            {auctionData.applicationDate}
-          </Text>
-          <Text style={styles.field}>
-            <FontAwesome5 name="clock" size={20} /> Application Deadline:{" "}
-            {auctionData.applicationDeadline}
+
+          <Text style={[styles.field, styles.reservePricefiled]}>
+            <FontAwesome5 name="tags" size={20} /> Reserve Price:{" "}
+            {auctionDetails.reservePrice}
           </Text>
           <Text style={styles.field}>
             <FontAwesome5 name="coins" size={20} /> Earnest Money Deposit:{" "}
-            {auctionData.earnestMoneyDeposit}
+            {auctionDetails.emd}
           </Text>
           <Text style={styles.field}>
-            <FontAwesome5 name="tags" size={20} /> Reserve Price:{" "}
-            {auctionData.reservePrice}
+            <FontAwesome5 name="university" size={20} /> Bank Name:{" "}
+            {auctionDetails.bank}
+          </Text>
+
+          <Text style={styles.field}>
+            <FontAwesome5 name="map-marker-alt" size={20} /> Locality:{" "}
+            {auctionDetails.locality}
+          </Text>
+          <Text style={styles.field}>
+            <FontAwesome5 name="city" size={20} /> City: {auctionDetails.city}
+          </Text>
+          <Text style={styles.field}>
+            <FontAwesome5 name="flag" size={20} /> State: {auctionDetails.state}
+          </Text>
+
+          <Text style={styles.field}>
+            <FontAwesome5 name="calendar-alt" size={20} /> Application Date:{" "}
+            {/* {auctionDetails.startDate || "NA"} */}
+            {dayjs(auctionDetails.startDate).format("MMM DD, YYYY")}
+          </Text>
+          <Text style={[styles.field, styles.appliationDeadlineFiled]}>
+            <FontAwesome5 name="clock" size={20} color="orange" /> Application
+            Deadline:{" "}
+            {auctionDetails.applicationDeadLine
+              ? auctionDetails.applicationDeadLin
+              : "NA"}
           </Text>
         </View>
       </View>
@@ -90,35 +123,193 @@ const AuctionDetails = () => {
         <Text style={styles.premiumTitle}>Premium Details</Text>
         {isPremiumUser ? (
           <View>
-            <Text style={styles.premiumField}>
-              <FontAwesome5 name="file-alt" size={20} /> Loan Account Number:{" "}
-              {auctionData.loanAccountNumber}
-            </Text>
-            <Text style={styles.premiumField}>
-              <FontAwesome5 name="user" size={20} /> Bank Contact Person:{" "}
-              {auctionData.bankContactPerson}
-            </Text>
-            <Text style={styles.premiumField}>
-              <FontAwesome5 name="map-marked-alt" size={20} /> Property Address:{" "}
-              {auctionData.propertyAddress}
-            </Text>
-            <Text style={styles.premiumField}>
-              <FontAwesome5 name="file-pdf" size={20} /> Documents:{" "}
-              {auctionData.documents}
-            </Text>
+            <View style={styles.detailRow}>
+              <FontAwesome5 name="file-alt" size={20} style={styles.icon} />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Loan Account Number</Text>
+                <Text style={styles.fieldValue}>
+                  {auctionDetails.loanAccountNumber}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5 name="user" size={20} style={styles.icon} />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}> Bank Contact Person</Text>
+                <Text style={styles.fieldValue}>
+                  {auctionDetails?.contactPerson?.name || "N/A"}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5 name="phone-alt" size={20} style={styles.icon} />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Bank Contact Person Phone</Text>
+                <Text style={styles.fieldValue}>
+                  {auctionDetails?.contactPerson?.phone || "N/A"}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5
+                name="map-marker-alt"
+                size={20}
+                style={styles.icon}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Property Address</Text>
+                <Text style={styles.fieldValue}>
+                  {auctionDetails.propertyAddress || "N/A"}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5 name="file-pdf" size={20} style={styles.icon} />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Documents Link</Text>
+                {auctionDetails?.documentLink?.length > 0 ? (
+                  auctionDetails?.documentLink.map((el, index) => (
+                    <Link key={index} href={el} style={{ color: "blue" }}>
+                      {el}
+                    </Link>
+                  ))
+                ) : (
+                  <Text style={styles.fieldValue}>No documents available</Text>
+                )}
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5
+                name="map-marked-alt"
+                size={20}
+                style={styles.icon}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Map</Text>
+              </View>
+            </View>
+
+            <View
+              style={{
+                height: 250,
+                shadowColor: "black",
+                shadowOffset: { width: -1, height: 1 },
+                shadowRadius: 18,
+              }}
+            >
+              <WebView
+                originWhitelist={["*"]}
+                source={{
+                  html: `<iframe src="${googleMapsEmbedUrl}" style="border:0;" allowFullScreen height="100%" width="100%" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>`,
+                }}
+              />
+            </View>
           </View>
         ) : (
           <View style={styles.lockedContent}>
+            <View style={styles.detailRow}>
+              <FontAwesome5
+                name="map-marker-alt"
+                size={20}
+                style={styles.icon}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Loan Account Number:</Text>
+                <Text
+                  style={{
+                    textShadowColor: "black",
+                    textShadowOffset: { width: -1, height: 1 },
+                    textShadowRadius: 18,
+                    color: "transparent",
+                  }}
+                >
+                  Test123AbcTest222555555
+                </Text>
+              </View>
+            </View>
+            <View style={styles.detailRow}>
+              <FontAwesome5 name="user" size={20} style={styles.icon} />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Bank Contact Person:</Text>
+                <Text
+                  style={{
+                    textShadowColor: "black",
+                    textShadowOffset: { width: -1, height: 1 },
+                    textShadowRadius: 18,
+                    color: "transparent",
+                  }}
+                >
+                  1234567890
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5
+                name="map-marker-alt"
+                size={20}
+                style={styles.icon}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Property Address:</Text>
+                <Text
+                  style={{
+                    textShadowColor: "black",
+                    textShadowOffset: { width: -1, height: 1 },
+                    textShadowRadius: 18,
+                    color: "transparent",
+                  }}
+                >
+                  123, xyz Abc XXXXXXX xxxxxXXXXXX XX
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.detailRow}>
+              <FontAwesome5
+                name="file-pdf"
+                size={20}
+                style={styles.icon}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.fieldTitle}>Documents:</Text>
+                <Text
+                  style={{
+                    textShadowColor: "black",
+                    textShadowOffset: { width: -1, height: 1 },
+                    textShadowRadius: 18,
+                    color: "transparent",
+                  }}
+                >
+                  www.asdsads.sadsda
+                </Text>
+              </View>
+            </View>
+
             <Text style={styles.premiumField}>
-              Loan Account Number: ************
+              {" "}
+              <FontAwesome5 name="file-pdf" size={20} /> Documents:{" "}
+              <Text
+                style={{
+                  textShadowColor: "black",
+                  textShadowOffset: { width: -1, height: 1 },
+                  textShadowRadius: 15,
+                  color: "transparent",
+                }}
+              >
+                Lorem
+              </Text>
             </Text>
             <Text style={styles.premiumField}>
-              Bank Contact Person: ********
+              <FontAwesome5 name="map-marked-alt" size={24} color="black" />{" "}
+              Location on Map :
             </Text>
-            <Text style={styles.premiumField}>
-              Property Address: ************
-            </Text>
-            <Text style={styles.premiumField}>Documents: **********</Text>
+
             <TouchableOpacity style={styles.upgradeButton}>
               <FontAwesome5 name="crown" size={18} color="white" />
               <Text style={styles.upgradeText}> Upgrade to Premium</Text>
@@ -131,12 +322,13 @@ const AuctionDetails = () => {
 };
 
 const styles = StyleSheet.create({
-  container: { padding: 20, backgroundColor: "#f8f9fa" },
+  container: { padding: 10, backgroundColor: "#f8f9fa" },
   card: {
     backgroundColor: "white",
-    padding: 25,
+    padding: 20,
     borderRadius: 15,
-    marginBottom: 20,
+    margin: 7,
+    marginBottom: 30,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.1,
@@ -150,13 +342,20 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   fieldContainer: { marginBottom: 10 },
-  field: { fontSize: 20, marginVertical: 6 },
+  field: { fontSize: 18, marginVertical: 6, fontWeight: "bold", opacity: 0.6 },
+  reservePricefiled: { color: "green" },
+  appliationDeadlineFiled: { color: "orange" },
   premiumCard: {
     position: "relative",
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    backgroundColor: "white",
     borderWidth: 1,
     borderColor: "gold",
-    padding: 30,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 3,
   },
   premiumTitle: {
     fontSize: 24,
@@ -165,25 +364,28 @@ const styles = StyleSheet.create({
     color: "#d4af37",
   },
   premiumField: {
-    fontSize: 18,
+    fontSize: 17,
     marginVertical: 6,
     fontWeight: "bold",
     opacity: 0.6,
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 10,
+    borderColor: "gold",
   },
   lockedContent: {
     backgroundColor: "rgba(255, 255, 255, 0.7)",
     padding: 10,
     justifyContent: "center",
-    alignItems: "center",
     borderRadius: 15,
-    // filter: "blur(8px)",
+    textAlign: "left",
   },
   upgradeButton: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "gold",
-    paddingVertical: 10,
+    padding: 10,
     borderRadius: 10,
     marginTop: 15,
   },
@@ -192,6 +394,48 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     marginLeft: 8,
+  },
+  // premiumCard: {
+  //   backgroundColor: "white",
+  //   borderWidth: 1,
+  //   borderColor: "gold",
+  //   padding: 20,
+  //   shadowColor: "#000",
+  //   shadowOffset: { width: 0, height: 4 },
+  //   shadowOpacity: 0.1,
+  //   shadowRadius: 6,
+  //   elevation: 3,
+  //   borderRadius: 10,
+  //   marginBottom: 10,
+  // },
+  // premiumTitle: {
+  //   fontSize: 24,
+  //   fontWeight: "bold",
+  //   marginBottom: 15,
+  //   color: "#d4af37",
+  //   textAlign: "center",
+  // },
+  detailRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+    color: "#d4af37",
+  },
+  textContainer: {
+    flex: 1,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  fieldValue: {
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#666",
   },
 });
 
